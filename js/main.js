f955e162de30e2bfee6ec2148bcf6596c70839b6
Xamzaev-Xumoyun <xamzaevxumoyun49@gmail.com>
2022-02-18 21:48:25 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var firstname = "Ismingizni kiriting"
+
+var firstname = prompt("Ismingizni kiriting") 
+var question1 = prompt("bootstrap ozi nma") 
+var question2 = prompt("bootstrapni 5 ta klasslarini sanab berin")
+var question3 = prompt("bootsrani caruselidan 2 ta yasasak 1 chisini bosganda 2chisiga tasr qilmasligi uchun nma qilish kerak")
+var question4 = prompt("bootstrapda imgni ortaga olib kelish uchun nma qilish kerak")
+var question5 = prompt("row degan classni ichida nechta col yozsa boladi ")
+var question6 = prompt("bootstaptagi alert nmani anglati")
+var question7 = prompt("bootstrapda breakpoint larni sanab bering") 
+var question8 = prompt("bootstrapda ul(ol) li lani qanday class bilan yozish kerak")
+var question9 = prompt("bootsrapda asosiy ranglar nechta")
+var question10 = prompt("button ga qizil rang bering bootsrapda"  , "<button type=button class=''></button> " )
+var question11 = prompt("bootsrat va css (sass , scss) responsivida qanday farq bor")
+
+
+
+
+ console.log(firstname + "\n" + question1 + "\n" + question2 + "\n" + question3 + "\n" + question5 + "\n" + question6 + "\n" + question7 + "\n" + question8 + "\n" + question9 + "\n" + question10 + "\n" + question11 );
